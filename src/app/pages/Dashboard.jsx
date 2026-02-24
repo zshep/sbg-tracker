318a8f288925889cc2fd2c02990a33313ba9649a
zshep <zsheperdigian@gmail.com>
2026-02-24 12:54:54 -0600

    refixed dashboard

--- src/app/pages/Dashboard.jsx
+++ src/app/pages/Dashboard.jsx
@@ -1,26 +1,22 @@
 import { useAuth } from "../context/AuthContext";
 
+import ClassSection from "../components/Dashboard/ClassSection";
+
 
 export default function Dashboard() {
+  const { user } = useAuth();
+  if (!user) return null;
 
-    const { user } = useAuth();
+  return (
+    <div>
+     
 
-    if(!user) return null;
+      <main style={{ flex: 1, padding: 16 }}>
+        <h2>Dashboard</h2>
+        <p>Hello user, {user.uid}</p>
 
-    return (
-        <div>
-            
-            <h3>Dashboard</h3>
-            <p>Hello user, {user.uid}</p>
-
-            <div>
-                <p>class Section</p>
-            </div>
-
-            <div>
-                <p>standars section </p>
-            </div>
-
-        </div>
-    )
+        <ClassSection />
+      </main>
+    </div>
+  );
 }
