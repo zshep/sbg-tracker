b01f47c5f76e0d876cb4e891d3327b75eefebf27
zshep <zsheperdigian@gmail.com>
2026-03-01 08:32:11 -0600

    removing main.jsx loaded

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,7 @@ import { AuthProvider } from "./app/context/AuthContext.jsx";
 import "./index.css";
 import App from "./App.jsx";
 
-console.log("MAIN.JSX LOADED");
-document.body.insertAdjacentHTML("afterbegin", "<div>main.jsx loaded</div>");
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
